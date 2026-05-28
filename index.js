212a760af3023ad51990060fbbbea0798dd663ea
harrychander99144-lab <harrychander99144@gmail.com>
2026-05-28 22:39:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const mineflayer = require('mineflayer');
 const readline = require('readline');
 
 // ===== CONFIG JO TU CHANGE KAR SAKTA HAI =====
-const SERVER_HOST     = 'in4-1.shulker.in';
-const SERVER_PORT     = 3095;
+const SERVER_HOST     = 'NetherMC_S3.aternos.me';
+const SERVER_PORT     = 47621;
 const BOT_USERNAME    = 'delux_ow';
 const MC_VERSION      = '1.21.11';
 const DEFAULT_COMMAND = '/login ajjubai94';
