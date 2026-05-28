09d93d9a847d16ceaa2bd0f4822b883bdbffb62b
harrychander99144-lab <harrychander99144@gmail.com>
2026-05-28 22:44:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const SERVER_HOST     = 'mumbai.firenode.lol';
 const SERVER_PORT     = 19158;
 const BOT_USERNAME    = 'delux_ow';
 const MC_VERSION      = '1.21.11';
-const DEFAULT_COMMAND = '/login ajjubai94';
+const DEFAULT_COMMAND = '/login ajjubai94 ajjubai94';
 
 // yahan se random chat ON/OFF karo
 const ENABLE_RANDOM_CHAT = true;    // true = on, false = off
